c0b0991669af6ebed0d2bae19fcae7e5d77994d7
shavkat <karimov2406s@mail.ru>
2024-01-31 19:34:59 +0500

    first commit

---
+++ No_Shtraf/src/Auth.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Auth.css'
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTER, REG_ROUTER } from './utils/const';
+
+const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTER
+
+
+
+    return (
+        <>
+            <div className="header">
+                
+            <img className='logo' src="./src/assets/logo.png" alt="logo" /><div className="title">
+                Штрафам - Да!
+                </div>
+            </div>
+            <div className="container">
+                {isLogin ? <p>
+                    Вход</p> : <p>Регистрация</p>}
+                <input type="text" />
+                <input type="password" />
+                <form action="#">
+                    <button className='btn_auth' type='sumbit'>
+                        {isLogin ? 'Войти' : 'Зарегистрироваться'}
+                        </button>
+                </form>
+                {isLogin ? <div>
+                    Нет аккаунта? <NavLink to={REG_ROUTER}>Зарегистрируйтесь!</NavLink>
+                </div> :
+                <div>
+                    Есть аккаунт? <NavLink to={LOGIN_ROUTER}>Войдите!</NavLink>
+                </div>}
+            </div>
+        </>
+    )
+}
+
+export default Auth
